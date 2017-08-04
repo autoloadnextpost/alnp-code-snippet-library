4f9723eef7f512f783a8f779ded496ce1a699474
Sébastien Dumont <mailme@sebastiendumont.com>
2017-08-04 13:26:11 +0200

    Create alnp-add-post-navigation-rel.js

---
+++ snippets/alnp-add-post-navigation-rel.js
@@ -0,0 +1,15 @@
+/**
+ * This snippet is useful if the theme post navigation is missing the use of rel="prev".
+ *
+ * Simply change "div.previous-post" to your themes previous post theme selector.
+ */
+
+// Runs on initial load of the post.
+jQuery( document ).ready( function() {
+	jQuery( 'div.previous-post' ).find( 'a' ).attr( 'rel', 'prev' );
+});
+
+// Runs when triggered.
+jQuery('body').on( 'alnp-post-loaded', function( e, post_title, post_url, post_ID, post_count ) {
+	jQuery( 'div.previous-post' ).find( 'a' ).attr( 'rel', 'prev' );
+});
