741afc8cbeb0454d91e1b5c899c2a9c009213fc6
Sébastien Dumont <mailme@sebastiendumont.com>
2018-06-02 14:45:09 +0200

    Update alnp-post-limit.js

--- snippets/alnp-post-limit.js
+++ snippets/alnp-post-limit.js
@@ -4,7 +4,7 @@
  * Set 'post_limit' to the amount of posts a session can load.
  */
 jQuery('body').on( 'alnp-post-loaded', function( e, post_title, post_url, post_ID, post_count ) {
-  var post_limit = 4;
+	var post_limit = 4;
 
   	if ( post_count == post_limit ) {
 		// Post count has reached maxiumum amount. No more posts will load.
